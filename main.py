17a7843e580bd67f9fc8c5cbf8dda80b041bd62f
Andreina Suescum <andreinasuescum@gmail.com>
2022-03-11 15:00:46 -0500

    snake

---
+++ main.py
@@ -0,0 +1,60 @@
+from turtle import Screen
+import time
+
+from food import Food
+from snake import Snake
+from score import Score
+
+# board game
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("orange")
+screen.title("Snake Game")
+#disable default Turtle animation
+screen.tracer(0)
+
+# create snake
+snake = Snake()
+
+# create food
+food = Food()
+
+# create score 
+score = Score()
+
+# listen keys
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+# snake animation
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # food collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
+        snake.extend()
+    
+    # wall collision
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
+        
+    # snake collision
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+        
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,54 @@
+from tkinter import RIGHT
+from turtle import Turtle
+
+#build snake body
+STARTING_POSITION = [(0,0),(-20,0),(-40,0)]
+
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+class Snake:
+    #Constructor
+    def __init__(self):
+         # save snake segments
+        self.segments = []
+        # method than created snake
+        self.create_snake()
+        # atribute head
+        self.head = self.segments[0]
+        
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+    
+    def add_segment(self, position):
+        snake_segment = Turtle("square")
+        snake_segment.color("white")
+        snake_segment.penup()
+        snake_segment.goto(position)
+        self.segments.append(snake_segment)
+    
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+    
+    def move(self):
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[seg_num -1].xcor()
+            new_y = self.segments[seg_num -1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
